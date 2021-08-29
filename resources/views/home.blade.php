3511d9bcdc69ad4817369edef4043fcc0ee1765c
Hery Sanjaya Situmorang <herysanjaya098@gmail.com>
2021-08-29 17:13:15 +0700

    project-kp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,17 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <title>HOME</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src='/js/logic.js'></script>
   </head>
   <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-          <a class="navbar-brand" href="#">LOGO</a>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <a href="#"> <img src="img/logokp.png" alt="" srcset=""></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -26,20 +23,59 @@
               <a class="nav-link" href="#">Contact Us</a>
             </div>
           </div>
+          <div class="icon">
+            <a href="#"> <img src="img/bold.png" alt="" class="shoping"> </a>
+            <a href="#"> <img src="img/Group.png" alt="" class="user"> </a>
+            <a href="#"> <img src="img/search.png" alt="" class="search"> </a>
+          </div>
         </div>
       </nav>
 
-    <h1>Hello, world!</h1>
+    <main>
+      
+      <div class="mainproduct">
+        <div class ="judul">
+          <h1 class="welcome-to-yasa">
+            <p align="left">WELCOME</p>
+            <p align="left">TO YASA</p>
+          </h1>
+          <p class="desc">Health & Aestetic | Official Web store</p>
+            <div class="button">
+              <a href="" class="learn_more">Learn More</a>
+            </div>
+        </div>
+        <img src="img/product.png" alt="" class="product">
+      </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+      <div class="mediasocial">
+        
+        <div style="margin: 20px; display: flex;">
+          <div style="display: flex; align-items: center; margin-right: 50px;">
+              <img src="img/tw.png" style="margin-right: 10px;" alt="">
+              <p>Twitter</p>
+          </div>
+          <div style="display: flex; align-items: center; margin-right: 50px;">
+              <img src="img/ig.png"  style="margin-right: 10px;" alt="">
+              <p>Instagram</p>
+          </div>
+          <div style="display: flex; align-items: center; margin-right: 50px;">
+              <img src="img/yt.png" style="margin-right: 10px;" alt="">
+              <p>Youtube</p>
+          </div>
+      </div>
+      </div>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+    </main>
+
+
+    <footer>
+      <h1> FOOTER</h1>
+    </footer>
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
-    -->
+    
+
+    
   </body>
 </html>
